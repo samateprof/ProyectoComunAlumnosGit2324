0f4d1ec3c94b371a668dc3e34efecbe15c775248
sebas <sebas@HP-Sebas>
2024-04-16 11:41:13 +0200

    Primer commit

---
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/SebastianRodriguez.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomunalumnos2324;
+
+/**
+ *
+ * @author sebas
+ */
+public class SebastianRodriguez {
+    public static void metodoMarian(){
+        System.out.println("Sebastián Rodríguez");
+    }
+}
