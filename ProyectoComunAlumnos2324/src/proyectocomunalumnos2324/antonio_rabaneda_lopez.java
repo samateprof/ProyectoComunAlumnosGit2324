d38eae1fd6196f6ffa2accad164fc24f46998449
MeLlamanRaba <rabaneda31524@gmail.com>
2024-04-16 11:43:35 +0200

    Actividad 4.3.4

---
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/antonio_rabaneda_lopez.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomunalumnos2324;
+
+/**
+ *
+ * @author Antonio
+ */
+public class antonio_rabaneda_lopez {
+    public void antonioRabanedaLopez(){
+        System.out.println("Metodo de Antonio Rabaneda Lopez");
+    }
+}
