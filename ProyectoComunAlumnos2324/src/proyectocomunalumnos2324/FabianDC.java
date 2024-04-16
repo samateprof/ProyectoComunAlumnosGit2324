52698120967c96717f79136e91960d7d5f4d2cee
Fabian <fabiyeli2020@gmail.com>
2024-04-16 11:43:12 +0200

    primer commit

---
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/FabianDC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocomunalumnos2324;
+
+/**
+ *
+ * @author maniana
+ */
+public class FabianDC {
+    
+    public void fabiDC(){
+        System.out.println("Método de Fabián");
+    }
+}
