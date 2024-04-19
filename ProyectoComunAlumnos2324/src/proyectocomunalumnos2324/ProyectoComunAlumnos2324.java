a4edb88c9d2c50a7886eef58c4c51574833b0f06
Sonia <Sonia@DESKTOP-3LHBG1B>
2024-04-19 14:05:44 +0200

    Nuevo

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
@@ -16,5 +16,6 @@ public class ProyectoComunAlumnos2324 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    // estamos allaniendo codigo nuevo
     
 }
