d70b437e931b25abe2617e178c5f99fa1249e3fb
Milagros <mila@example.com>
2024-04-23 11:40:58 +0200

    commit

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
@@ -15,6 +15,7 @@ public class ProyectoComunAlumnos2324 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Milagros Rubio ");
     }
     
 }
