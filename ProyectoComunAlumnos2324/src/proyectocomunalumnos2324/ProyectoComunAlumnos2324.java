30f293b2beb530d30911ea3eb9d880d38ee3d235
repaso <repaso@lmsgi.edu>
2024-04-23 11:34:52 +0200

    Uso el método Dalian <3

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyectocomunalumnos2324;
 
-/**
- *
- * @author sonia
- */
 public class ProyectoComunAlumnos2324 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	public static void main(String[] args) {
+
+	Dalian d1 = new Dalian();
+
+	d1.Dalian();
+
+	}
+
 }
