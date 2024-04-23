08df4faafd7bda357adfe9cb55d3b822125406fb
Marian <Marian@ASUS-A15>
2024-04-23 11:45:56 +0200

    Actividad 4.3.5 Entornos Marian. Metodo añadido

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
@@ -14,7 +14,8 @@ public class ProyectoComunAlumnos2324 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Llamada al método de Marian
+        ClaseIonMarianGeorgian.metodoMarian();
     }
     
 }
