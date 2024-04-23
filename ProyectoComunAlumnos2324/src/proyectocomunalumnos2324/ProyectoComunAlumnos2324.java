fee37487e4659a2138fe35ed6166fbbc42eccbdf
Alejandro Miras <alejandromirasandujar@gmail.com>
2024-04-23 11:35:03 +0200

    Añado información al main

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
@@ -14,7 +14,7 @@ public class ProyectoComunAlumnos2324 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Alejandro Miras Andújar");
     }
     
 }
