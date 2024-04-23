faaf358e9f6a3c05eeef759e8860a288693464dd
Alejandro Miras <alejandromirasandujar@gmail.com>
2024-04-23 11:41:50 +0200

    He cambiado la clase

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/MirasAndujarAlejandro.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/MirasAndujarAlejandro.java
@@ -12,6 +12,7 @@ public class MirasAndujarAlejandro {
     
     public void AlejandroMiras(){
         System.out.println("Método de Alejandro Miras Andújar");
+        System.out.println("Alex estuvo aquí");
     }
     
 }
