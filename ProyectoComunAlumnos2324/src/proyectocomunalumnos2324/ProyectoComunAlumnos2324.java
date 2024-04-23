e085d1291ff069fc75041b1a4af8d027cb86f379
repaso <repaso@lmsgi.edu>
2024-04-23 11:34:56 +0200

    uso mi metodo Jesus

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/ProyectoComunAlumnos2324.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyectocomunalumnos2324;
 
-/**
- *
- * @author sonia
- */
 public class ProyectoComunAlumnos2324 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Jesus j1 =new Jesus();
+        j1.Jesus();
+
     }
     
 }
