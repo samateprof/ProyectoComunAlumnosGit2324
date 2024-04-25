78a9198f5db8e97c4d1ab32a2a0062ea320a3f3e
sebas <sebas@HP-Sebas>
2024-04-25 11:46:22 +0200

    9veno commit

--- ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/SebastianRodriguez.java
+++ ProyectoComunAlumnos2324/src/proyectocomunalumnos2324/SebastianRodriguez.java
@@ -5,7 +5,7 @@ public class SebastianRodriguez {
 
     public static void metodoSebastian(){
         System.out.println("Sebastián Rodríguez");
-        System.out.println("Ha estado aqui...");
+        System.out.println("El que tenga miedo a morir\n\tQue no nazca.");
     }
 
 }
